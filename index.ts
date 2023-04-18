e27f2dce6dad5c17f8521a024729993e547555cf
MateuMiralles <mateumiralles714@gmail.com>
2023-04-18 06:36:25 +0200

    - API Response

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+fetch(`https://api.football-data.org/v4/competitions/PL/scorers/?limit=50`, {
+  headers: {
+    method: "GET",
+    "X-Auth-Token": "2594dcaf6afd43fd88946f67658232e6",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data));

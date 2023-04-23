4923f80b4f8c874fb5e6cf4badf47ddb29a2148f
MateuMiralles <mateumiralles714@gmail.com>
2023-04-23 05:40:01 +0200

    - List page styles

--- renderer.ts
+++ renderer.ts
@@ -1,12 +1,13 @@
 import { Scorer } from "./scorers.js";
 
-const headHtml = (title: String) => {
+const headHtml = (title: String, style: String) => {
   return `
     <head>
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>${title}</title>
+      <link rel="stylesheet" href="${style}.css">
     </head>
     `;
 };
@@ -15,11 +16,12 @@ export const renderListPage = (scorers: Array<Scorer>) => {
   return `
     <!DOCTYPE html>
     <html lang="en">
-    ${headHtml("Europe Top Scorers")}
+    ${headHtml("Europe Top Scorers", 'stylesList')}
     
     <body>
     <h1> Europe's Top Scorers </h1>
-      ${renderList(scorers)}
+     <div class='heading'> <span>RANKING</span> <span>PLAYER</span> <span>GOALS</span> </div>
+     <div> ${renderList(scorers)} </div>
     </body>
     </html>
     `;
@@ -27,19 +29,20 @@ export const renderListPage = (scorers: Array<Scorer>) => {
 
 const renderList = (scorers: Array<Scorer>) => {
   let htmlListItem = "";
-
+  let i = 1;
   for (const scorer of scorers) {
-    htmlListItem += `<a href='scorers/${scorer.joinedName}.html'> <li> ${scorer.name} -  ${scorer.goals} -  ${scorer.assists}</li> </a>`;
+    htmlListItem += `<a href='scorers/${scorer.joinedName}.html'> <div> <div>#${i}</div> <div class='item-col'> <span class='item-name'>${scorer.name}</span> <span class='item-team'>${scorer.team}</span></div>   <span class='item-goals'>${scorer.goals}</span> </div> </a>`;
+    i++;
   }
 
-  return `<ol> ${htmlListItem} </ol>`;
+  return `${htmlListItem}`;
 };
 
 export const renderDetailPage = (scorer: Scorer) => {
   return `
     <!DOCTYPE html>
     <html lang="en">
-    ${headHtml(`${scorer.name} - Details`)}
+    ${headHtml(`${scorer.name} - Details`, '')}
     </head>
     <body>
       <h1> ${scorer.name}</h1>
